a4df31b9fd1d319510962d81608b6a9082366aba
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2014-10-05 00:22:27 -0700

    Naive echobot

---
+++ Gemfile
@@ -0,0 +1,2 @@
+# Tell the library to use karl's chat-adapter library from github.
+gem 'chat-adapter', :git => 'git@github.com:macobo/chat-adapter.git'
---
+++ bot.rb
@@ -0,0 +1,35 @@
+# use karl's chat-adapter library
+require 'chat-adapter'
+# also use the local HerokuSlackbot class defined in heroku.rb
+# require './heroku'
+
+# if we're on our local machine, we want to test our bot via shell, but when on
+# heroku, deploy the actual slackbot.
+bot = ChatAdapter::Shell.new(nick: 'echobot')
+log = ChatAdapter.log
+
+# Do this thing in this block each time the bot hears a message:
+bot.on_message do |message, info|
+  # ignore all messages not directed to this bot
+  unless message.start_with? 'echobot: '
+    next # don't process the next lines in this block
+  end
+
+  # Conditionally send a direct message to the person saying whisper
+  if message == 'echobot: whisper'
+    # log some info - useful when something doesn't work as expected
+    log.debug("Someone whispered! #{info}")
+    # and send the actual message
+    bot.direct_message(info[:user], "whisper-whisper")
+  end
+
+  # split the message in 2 to get what was actually said.
+  botname, command = message.split(': ', 2)
+
+  # answer the query!
+  # this stupid bot simply echoes the message back
+  "@#{info[:user]}: #{command}"
+end
+
+# actually start the bot
+bot.start!
